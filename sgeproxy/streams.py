ee26f993b93d5e02ba3c15a66efa91fb0eac1004
Cyril Lugan <cyril@lugan.fr>
2022-07-09 14:47:09 +0200

    WIP new streams

--- sgeproxy/streams.py
+++ sgeproxy/streams.py
@@ -1,4 +1,6 @@
-from typing import Dict, Iterable, Optional
+import logging
+import statistics
+from typing import Dict, Iterable, Optional, List, Tuple
 import xml.etree.ElementTree as ET
 import datetime as dt
 import pytz
@@ -222,5 +224,130 @@ class R151:
                 name = base_name + "/power/apparent/max"
                 unit = "VA"
                 record = Record(name, time, unit, value)
+
+                yield record
+
+# Multiple files per zip archive
+# Multiple PRMs per files
+class R50:
+    def __init__(self, xml_doc: str) -> None:
+        self.doc = ET.parse(xml_doc)
+
+    def records(self) -> Iterable[Record]:
+
+        header = _find_element(self.doc, "En_Tete_Flux")
+        period_minutes = int(_find_text(header, "Pas_Publication"))
+
+        # Spec says its always 30 min but I thought this could be an hour too
+        assert period_minutes == 30
+
+        for prm in self.doc.findall("./PRM"):
+            usage_point = _find_text(prm, "Id_PRM")
+            # FIXME(cyril) Nothing specified in this stream?
+            # TODO(cyril) Check on a production usage point
+            # (check in stream doc and commande collecte before)
+            direction = "consumption"
+
+            name = f"urn:dev:prm:{usage_point}_{direction}/power/active/raw"
+
+            # Just to check time period
+            records: List[Tuple[dt.datetime, int]] = []
+
+            for pdc in prm.findall("./Donnees_Releve/PDC"):
+                datetime = _find_text(pdc, "H")
+                value = int(_find_text(pdc, "V"))
+                caution = int(_find_text(pdc, "IV"))
+
+                datetime = dt.datetime.fromisoformat(datetime)
+
+                # Data from file is stamped at the end of periods,
+                # quoalise timestamp them at the begining
+                datetime = datetime - dt.timedelta(minutes=period_minutes)
+
+                records.append((datetime, value))
+            
+            datetimes = sorted(datetime for datetime, _ in records)
+            periods = [j-i for i in datetimes[:-1] for j in datetimes[1:]]
+            if periods:
+                period_median = statistics.median([int(p.total_seconds()/60) for p in periods])
+                assert period_median == period_minutes
+
+            for datetime, value in records:
+                record = Record(name, datetime, "W", value) 
+
+                yield record
+
+class R4x:
+    def __init__(self, xml_doc: str) -> None:
+        self.doc = ET.parse(xml_doc)
+
+    def records(self) -> Iterable[Record]:
+
+        header = _find_element(self.doc, "Entete")
+        nature = _find_text(header, "Nature_De_Courbe_Demandee")
+        if nature == "Brute":
+            nature = "raw"
+        else:
+            raise RuntimeError(f"Nature {nature} is not supported yet")
+        
+        body = _find_element(self.doc, "Corps")
+        usage_point = _find_text(body, "Identifiant_PRM")
+
+        for curve in body.findall("./Donnees_Courbe"):
+
+            unit = _find_text(curve, "Unite_Mesure")
+
+            period = int(_find_text(curve, "Granularite"))
+            assert period == 10 # From spec
+
+
+            direction = _find_text(curve, "Grandeur_Metier")
+            if direction == "CONS":
+                direction = "consumption"
+            elif direction == "PROD":
+                direction = "production"
+
+            measurement = _find_text(curve, "Grandeur_Physique")
+            if measurement == "EA":
+                name = "power/active"
+                assert unit == "kW"
+                unit = "Wh" # Value will be converted
+            elif measurement == "ERC":
+                name = "power/capacitive"
+            elif measurement == "ERI":
+                name = "power/inductive"
+            elif measurement == "E":
+                name = "voltage"
+            else:
+                raise RuntimeError(f"Unexpected Grandeur {name}")
+
+            name = f"urn:dev:prm:{usage_point}_{direction}/{name}/{nature}"
+
+            for point in curve.findall("./Donnees_Point_Mesure"):
+
+                datetime = dt.datetime.fromisoformat(point.attrib["Horodatage"])
+                value = int(point.attrib["Valeur_Point"])
+                status = point.attrib["Statut_Point"]
+
+                if status != "R":
+                    # R : Réel
+                    # H : Puissance reconstituée
+                    # P : Puissance reconstituée et Coupure Secteur
+                    # S : Coupure secteur
+                    # T : Coupure Secteur courte
+                    # F : Début coupure secteur
+                    # G : Fin de coupure secteur
+                    # E : Estimé
+                    # C : Corrigé
+                    # K : Calculé, point de courbe issu d’un calcul basé sur d’autres courbes de charges
+                    # D : importé manuellement par le métier Enedis
+                    logging.warn(f"status {status} is not handled yet")
+                    continue
+
+                if measurement == "EA":
+                    # sge tiers proxy uses W
+                    value = value * 1000
+                
+                record = Record(name, datetime, unit, value)
 
                 yield record
